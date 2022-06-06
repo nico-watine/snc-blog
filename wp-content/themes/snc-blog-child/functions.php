dbd0dc2843547fa3b735a1314a2bc8cfc27d07ff
Nico Watine <nico@studioncreations.com>
2022-06-06 11:34:49 -0500

    Move some functions to the child

--- wp-content/themes/snc-blog-child/functions.php
+++ wp-content/themes/snc-blog-child/functions.php
@@ -9,3 +9,31 @@ function snc_blog_child_enqueue_styles() {
     array('parent-style')
   );
 }
+
+// Remove emoji scripts and styles
+remove_action('wp_head', 'print_emoji_detection_script', 7 );
+remove_action('admin_print_scripts', 'print_emoji_detection_script' );
+remove_action('wp_print_styles', 'print_emoji_styles' );
+remove_action('admin_print_styles', 'print_emoji_styles' );
+
+remove_action('wp_head', 'wp_generator'); // Remove WordPress Generator version number
+remove_action('wp_head', 'wlwmanifest_link'); // Remove Windows Live Writer Manifest Link
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0); // Remove shortlink
+remove_action('wp_head', 'wp_resource_hints', 2, 99 );
+
+// Add Custom CSS to Admin Area
+add_action('admin_head', 'custom_dashboard_css');
+
+function custom_dashboard_css() {
+  // Hide all plugin registration notices after installing WP-Optimize Premium
+  echo '<style>
+    .updated#udmupdater_not_connected {
+      display: none !important;
+      visibility: hidden !important;
+    }
+    [data-slug="wp-optimize-premium"] + tr {
+      display: none !important;
+      visibility: hidden !important;
+    }
+  </style>';
+}
