7b193746fdacc7e295ed38639f24058bb43df0b9
Nico Watine <nico@studioncreations.com>
2023-01-31 18:55:18 -0600

    Change url value

--- wp-content/themes/snc-blog/header.php
+++ wp-content/themes/snc-blog/header.php
@@ -21,9 +21,9 @@
 		<?php the_custom_logo(); ?>
 		<?php
 		if ( is_front_page() && is_home() ) : ?>
-			<h1 class="monospace header__title"><a href="https://studioncreations.com/blog/" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h1 class="monospace header__title"><a href="/blog/" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 		<?php else : ?>
-			<p class="monospace header__title"><a href="https://studioncreations.com/blog/" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<p class="monospace header__title"><a href="/blog/" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 		<?php
 		endif;
 
