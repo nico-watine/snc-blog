d15965da726ef95d8e42ef1d5690da491c76eccf
Nico Watine <nico@studioncreations.com>
2023-02-14 12:02:08 -0600

    Remove jquery cdn scripts

--- wp-content/themes/snc-blog/web-development-template.php
+++ wp-content/themes/snc-blog/web-development-template.php
@@ -243,9 +243,7 @@
 	</button>
 </div>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="/js/vendor/jquery/3-6-0/jquery-min.js"><\/script>')</script>
-<script src="/js/app-min.js?=2022-10-03T16:28:30Z"></script>
+<script src="/js/app-min.js?=2023-02-14T18:00:24Z"></script>
 
 </body>
 </html>
