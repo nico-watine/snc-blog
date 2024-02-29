2ed0448867908cd32ed9d942e819f05c9cfc1169
Nico Watine <nico@studioncreations.com>
2024-02-28 21:00:47 -0600

    More js

--- lightToggle/js/src/lighttoggle.js
+++ lightToggle/js/src/lighttoggle.js
@@ -47,3 +47,18 @@ const myDarkMode = {
 document.addEventListener('DOMContentLoaded', () => {
 	myDarkMode.init();
 });
+
+(function() {
+  const darkMode = localStorage.darkMode === 'true';
+  if (darkMode) {
+    document.querySelector('body').classList.add('is-dark');
+
+    // activate the toggle
+    document.addEventListener('DOMContentLoaded', () => {
+      const $toggles = document.querySelectorAll('.dark-toggle input[type="checkbox"]');
+      $toggles.forEach(($t) => {
+        $t.checked = true;
+      });
+    });
+  }
+})();
